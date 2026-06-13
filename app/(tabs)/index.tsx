2060fe22c8e605543f88d8292239e064886b675f
Ankit <178733212+AnkitB92@users.noreply.github.com>
2026-06-13 22:58:16 +0530

    Update home screen with ImageViewer

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import ImageViewer from '@/components/ImageViewer';
+import { StyleSheet, View } from 'react-native';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About page
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
     </View>
   );
 }
@@ -16,15 +17,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    color: '#fff',
-    textDecorationLine: 'underline',
+  imageContainer: {
+    flex: 1,
   },
 });
